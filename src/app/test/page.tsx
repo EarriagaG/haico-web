efdc184b7772a08ddadee39d31357127f2b03190
EarriagaG <eduardo@haico.mx>
2025-03-27 18:09:28 -0600

    Configuración de Tailwind corregida

---
+++ src/app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+    return (
+      <div className="flex items-center justify-center h-screen bg-yellow-100">
+        <h1 className="text-4xl font-bold">PRUEBA DE CENTRADO</h1>
+      </div>
+    );
+  }
+  
